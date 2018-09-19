9388f927a46aac3c5bd768f4ad411e8ce9e5e65f
nico vogel <nico-vogel97@outlook.de>
2018-09-19 22:07:30 +0200

    test

--- node/app.js
+++ node/app.js
@@ -7,23 +7,23 @@ client.connect();
 
 
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.send('die views sind unter der url /v1 bis /v4 zu finden');
 });
 
-app.get('/v1', function(req, res){
+app.get('/v1', function (req, res) {
     fetchAllOfType(res, 'arzt_overview');
 });
 
-app.get('/v2', function(req, res){
+app.get('/v2', function (req, res) {
     fetchAllOfType(res, 'fachbereich_overview');
 });
 
-app.get('/v3', function(req, res){
+app.get('/v3', function (req, res) {
     fetchAllOfType(res, 'arzt_overview_time');
 });
 
-app.get('/v4', function(req, res){
+app.get('/v4', function (req, res) {
     fetchAllOfType(res, 'angestellten_overview');
 });
 
@@ -31,9 +31,9 @@ app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
 
-function fetchAllOfType(res, type){
+function fetchAllOfType(res, type) {
     var options = {
-       sql : 'SELECT * FROM ' + type
+        sql: 'SELECT * FROM ' + type
     }
     console.log("query = " + options.sql);
     executeQuery(options, res, createViewTable);
@@ -42,32 +42,33 @@ function fetchAllOfType(res, type){
 
 
 function executeQuery(options, res, cb) {
-    if(!options || !options.sql)
-        throw (new Error('Invalid sql statement')); 
-    
+    if (!options || !options.sql)
+        throw (new Error('Invalid sql statement'));
+
     console.log("client is null: " + !client);
     client.query(options.sql, function (err, result, fields) {
-    if (err) {
-        console.log(err);
-        throw err;
-    }
+        if (err) {
+            console.log(err);
+            throw err;
+        }
         cb(res, result);
     });
 }
 
-const createViewTable = function(res, result){
+const createViewTable = function (res, result) {
     res.write("<table>");
     res.write("<tr>");
-    for(var column in result[0]){
-        res.write("<td><label>" + column + "</label></td>");
+    for (var field in result.fields) {
+        res.write("<td><label>" + field.name + "</label></td>");
     }
     res.write("</tr>");
-    for(var row in result){
-        res.write("<tr>");
-        for(var column in result[row]){
-            res.write("<td><label>" + result[row][column] + "</label></td>");       
-        }
-        res.write("</tr>");         
+    /*
+    for(var row in result.rows){
+        res.write("<td><label>" + result[row][column] + "</label></td>");       
+    }
+    */
+    for (var row in result.rows) {
+        console.log(row);
     }
     res.write("</table>");
     res.end();
